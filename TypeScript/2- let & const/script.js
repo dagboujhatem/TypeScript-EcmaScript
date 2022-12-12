fbd366270480d075eb18e8f102d3267c5f72a89e
dagbouj hatem <dagboujhatem@gmail.com>
2022-12-12 19:40:21 +0100

    let & const dans ES

--- TypeScript/2- let & const/script.js
+++ TypeScript/2- let & const/script.js
@@ -2,23 +2,17 @@
 // 1. Utilisez toujours les "const" pour les variables dans tous le script.
 // 2. Utiliser le "let" si vous avez une affectation dans votre script.
 // 3. Evitez d'utiliser "var" le maximum, utilisez-le si vous n'avez pas d'autres solution.
-
-const NAME = "Hatem";
-const LAST_NAME = "DAGHBOUJ";
-const isShow = true;
-
-if(isShow) {
-    const FULL_NAME = `${NAME} ${LAST_NAME}`; 
+var NAME = "Hatem";
+var LAST_NAME = "DAGHBOUJ";
+var isShow = true;
+if (isShow) {
+    var FULL_NAME = "".concat(NAME, " ").concat(LAST_NAME);
     console.log(FULL_NAME); // Output: "Hatem DAGHBOUJ"
 }
 // console.log(FULL_NAME); // ERROR: Cannot find name 'FULL_NAME'.
-
-
 // Using the let to update some variable
-
-if(true){
-    let hasError = false;
-    console.log(hasError); // Output: false
+if (true) {
+    var hasError = false;
+    console.log(hasError); // Output: 
 }
-
 // console.log(hasError); // ERROR: Cannot find name 'hasError'.
