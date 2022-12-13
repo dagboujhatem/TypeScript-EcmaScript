7279dd56e52e749e446cc59a8e1a18f44d73aedc
dagbouj hatem <dagboujhatem@gmail.com>
2022-12-13 12:08:18 +0100

    arrow function

--- TypeScript/4- arrow function/script.js
+++ TypeScript/4- arrow function/script.js
@@ -1,18 +1,18 @@
-
-/* 90s */
+/* 90s ==> 1997(ES1), 1998(ES2), 1999(ES3) */
 // function sayHi(userName) {
 //     console.log('Hi ' + userName);
 // }
 
-// /* 2000s */
+// /* 2000s => 2009(ES5)*/
 // var sayHi = function (userName) {
 //     console.log('Hi ' + userName);
 // };
 
-// /* 2015 => (ES6) */
-// const sayHi = (userName) => {
-//     console.log('Hi ' + userName);
-// };
+/* 2015 => (ES6) */
+const sayHi = (userName) => {
+    console.log('Hi ' + userName); 
+    // IMPORTANT: No more issues with the "this" keyword!
+};
 
 // /* 2018 => (ES9) */
 // export function sayHi(userName) {
