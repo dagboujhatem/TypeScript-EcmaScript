94389beb2575b843f63dbdcad830cd1893a6fe8b
dagbouj hatem <dagboujhatem@gmail.com>
2022-12-13 11:43:57 +0100

    template string

--- TypeScript/5- template string/script.js
+++ TypeScript/5- template string/script.js
@@ -1,5 +1,8 @@
-function load() {
+function loading() {
     var currentDate = new Date();
+    // Bad way : 
+    // const message = "The current date is: " + currentDate.getDate() + "/" + currentDate.getMonth() + "/" + currentDate.getFullYear();
     var message = "The current date is: ".concat(currentDate.getDate(), "/").concat(currentDate.getMonth(), "/").concat(currentDate.getFullYear());
     alert(message);
 }
+// export {}; // 👈️ make file ES Module
--- TypeScript/5- template string/script.ts
+++ TypeScript/5- template string/script.ts
@@ -1,7 +1,9 @@
-function load() {
+function loading(): void {
     const currentDate = new Date();
     // Bad way : 
     // const message = "The current date is: " + currentDate.getDate() + "/" + currentDate.getMonth() + "/" + currentDate.getFullYear();
     const message = `The current date is: ${currentDate.getDate()}/${currentDate.getMonth()}/${currentDate.getFullYear()}`;
     alert(message);
 }
+
+// export {}; // 👈️ make file ES Module
