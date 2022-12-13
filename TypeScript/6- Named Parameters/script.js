31235040c01ef4ddbaac4e3957140a403d8c0876
dagbouj hatem <dagboujhatem@gmail.com>
2022-12-13 15:03:41 +0100

    Named Parameters

--- TypeScript/6- Named Parameters/script.js
+++ TypeScript/6- Named Parameters/script.js
@@ -1,26 +1,13 @@
 // Les paramètres ordonnées nuisent à la lisibilité du code et au refactoring.
 // Malheureusement, les named parameters n'existent pas mais une solution de contournement native est possible grâce au destructuring.
 // Link of demo: https://guide-angular.wishtack.io/ecmascript-6+/named-parameters
-
-// interface ICostomer {
-//     firstName: string;
-//     lastName: string;
-//     email: string;
-//     phoneNumber: string;
-// }
-
-class Customer {
-    firstName: string;
-    lastName: string;
-    email: string;
-    phoneNumber: string;
-    // Named Parameters avec un seul paramètre (args).
-    constructor(args: any = {}) {
-        // Destructuring of args
-        const {firstName, lastName, email, phoneNumber = null} = args;
+var Customer = /** @class */ (function () {
+    function Customer(args) {
+        var firstName = args.firstName, lastName = args.lastName, email = args.email, _a = args.phoneNumber, phoneNumber = _a === void 0 ? null : _a;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
-}
+    return Customer;
+}());
