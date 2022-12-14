3b82dd47289919ebfe542b53a387931e2707790e
dagbouj hatem <dagboujhatem@gmail.com>
2022-12-14 17:26:33 +0100

    custom types

--- TypeScript/project/src/les types/custum type.ts
+++ TypeScript/project/src/les types/custum type.ts
@@ -3,3 +3,7 @@ type User = {firstname: string, lastName: string}
 
 // utilisation d'un type personalisé
 const userInstance : User = {firstname: 'Jhon', lastName: 'Doe'}
+
+// Dans le pratique
+type DateString = string;
+type Id = number | string;
