5e58836734b9555425be189a9f1c1ae66b374420
dagbouj hatem <dagboujhatem@gmail.com>
2022-12-15 16:07:56 +0100

    create new decorator

--- TypeScript/project-3/src/main.ts
+++ TypeScript/project-3/src/main.ts
@@ -6,15 +6,73 @@
  *    4. Class Decorators are applied for the class.
  */
 
-function CustomElement(name : string){
-    return function(constructor: typeof HTMLElement){
+
+/**
+ *   Class Decorators are applied for the class.
+ */
+
+function CustomElement(name: string) {
+    return function (constructor: typeof HTMLElement) {
         customElements.define(name, constructor)
     }
 }
 
 @CustomElement('hello-world')
 export class Demo extends HTMLElement {
-    connectedCallback (){
+    connectedCallback() {
         this.innerHTML = '<h1>Hello world</h1>'
     }
 }
+
+/**
+ *  Property Decorators are applied for each instance member.
+ */
+
+function Constraint({ min, max }: { min: number, max: number }) {
+    // return function(target: User, key: keyof User){ }
+    // return function(target: User, key: 'age'){ }
+    return function <T>(target: T, key: keyof T) {
+        let value = target[key] as any;
+        // console.log({ min, max }); // Output: {min: 0, max: 100} 
+        // console.log(value); // Output: undefined 
+        // console.log(key); // Output: age
+
+        // Getter & Setter
+        const getter = () => value
+        const setter = (newValue: unknown) => {
+            console.log(value)
+            console.log(newValue)
+            console.log(typeof newValue)
+            if (typeof newValue === 'number') {
+                if(newValue > min && newValue < max){
+                    value = newValue;
+                    return;
+                }else{
+                    throw new Error(`On attend un nombre entre ${min} et ${max}.`)
+                }
+            }else{
+                throw new Error(`On attend un nombre.`)
+            }
+        }
+
+        // Redefine the target object property
+        Object.defineProperty(target, key, {
+            set: setter,
+            get: getter
+        });
+
+    }
+}
+
+class User {
+    @Constraint({
+        min: 0,
+        max: 100
+    })
+    public age = 200;
+}
+
+const u = new User();
+
+console.log(u.age) // getter
+u.age = 15; // setter
